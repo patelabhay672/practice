8cacb462947a0f98b7d9116dd6efbd228eae1584
patelabhay672 <patelabhay0302@gmail.com>
2025-05-14 18:24:32 +0530

    Side ScrollTrigger complete

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-gsap.to("#page2 img",{
-  width:"100%",
-  scrollTrigger:{
+gsap.to("#page2 img", {
+  width: "100%",
+  scrollTrigger: {
     trigger: "#page2",
-    scroller:'body',
+    scroller: "body",
     markers: true,
-    start:"top 0",
-    end:"top -100%",
-    scrub:2,
-    pin:true
-  }
-})
+    start: "top 0",
+    end: "top -100%",
+    scrub: 2,
+    pin: true,
+  },
+});
+
+gsap.to("#page3 h1", {
+  transform: "translateX(-150%)",
+  scrollTrigger: {
+    trigger: "#page3",
+    scroller: "body",
+    markers: true,
+    scrub: 4,
+    start: "top 30%",
+    end: "top 2%",
+  },
+});
